5d773ae0348b62930836992734f6b5b67dc30549
snow520520 <leeseolhang0624@gmail.com>
2025-10-13 18:26:36 +0900

    [Design]header 메뉴 추가

--- final_atelierism_front/src/common/Header.jsx
+++ final_atelierism_front/src/common/Header.jsx
@@ -7,9 +7,29 @@ const Header = () => {
         <div className="logo">
           <Link to="/">Atelierism</Link>
         </div>
+        <MainNavi />
       </div>
     </header>
   );
 };
-
+const MainNavi = () => {
+  return (
+    <nav className="nav">
+      <ul>
+        <li>
+          <Link to="#">메뉴1</Link>
+        </li>
+        <li>
+          <Link to="#">메뉴2</Link>
+        </li>
+        <li>
+          <Link to="#">메뉴3</Link>
+        </li>
+        <li>
+          <Link to="#">메뉴4</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 export default Header;
