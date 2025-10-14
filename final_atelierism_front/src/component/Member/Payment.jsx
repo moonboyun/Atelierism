160fcf72c5a03ff4e2bd7b4ca7126818184fbf68
조성빈 <binny0903@gmail.com>
2025-10-14 14:15:00 +0900

    [Docs] 일반회원용 결제내역 페이지 작성 진행중

---
+++ final_atelierism_front/src/component/Member/Payment.jsx
@@ -0,0 +1,44 @@
+const Payment = () => {
+  return (
+    <section className="payment-wrap">
+      <div className="page-title">결제 내역</div>
+      <table className="page-content-wrap">
+        <tr>
+          <th>
+            <img src="/image/default_img2.png" />
+          </th>
+          <th>거실 인테리어</th>
+          <td>가격 : </td>
+          <td>디자이너 이름 : </td>
+          <td>인테리어 이유 : </td>
+          <td>프로필 링크 : </td>
+          <td>결제일 : </td>
+        </tr>
+        <tr>
+          <th>
+            <img src="/image/default_img2.png" />
+          </th>
+          <th>거실 인테리어</th>
+          <td>가격 : </td>
+          <td>디자이너 이름 : </td>
+          <td>인테리어 이유 : </td>
+          <td>프로필 링크 : </td>
+          <td>결제일 : </td>
+        </tr>
+        <tr>
+          <th>
+            <img src="/image/default_img2.png" />
+          </th>
+          <th>거실 인테리어</th>
+          <td>가격 : </td>
+          <td>디자이너 이름 : </td>
+          <td>인테리어 이유 : </td>
+          <td>프로필 링크 : </td>
+          <td>결제일 : </td>
+        </tr>
+      </table>
+    </section>
+  );
+};
+
+export default Payment;
