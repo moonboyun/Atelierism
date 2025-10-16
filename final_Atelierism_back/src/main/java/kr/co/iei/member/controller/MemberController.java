e8840d9cedbb0b0913ffba7402f8e57df24bad77
조성빈 <binny0903@gmail.com>
2025-10-16 15:54:38 +0900

    [Docs]Backend 멤버컨트롤러에 sysout 삭제

--- final_Atelierism_back/src/main/java/kr/co/iei/member/controller/MemberController.java
+++ final_Atelierism_back/src/main/java/kr/co/iei/member/controller/MemberController.java
@@ -5,9 +5,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import kr.co.iei.member.model.dto.LoginMemberDTO;
 import kr.co.iei.member.model.dto.MemberDTO;
 import kr.co.iei.member.model.service.MemberService;
 
@@ -17,12 +20,15 @@ import kr.co.iei.member.model.service.MemberService;
 public class MemberController {
 	@Autowired
 	private MemberService memberService;
-	
+	@PostMapping(value="/login")
+	public ResponseEntity<MemberDTO> login(@RequestBody MemberDTO member){
+		MemberDTO m = memberService.login(member);
+		return ResponseEntity.ok(m);
+
+	}
 	@GetMapping(value="/{memberId}")
 	public ResponseEntity<MemberDTO> selectOneMember(@PathVariable String memberId){
-		System.out.println(memberId);
 		MemberDTO member = memberService.selectOneMember(memberId);
-		System.out.println(member);
 		return ResponseEntity.ok(member);
 	}
 
