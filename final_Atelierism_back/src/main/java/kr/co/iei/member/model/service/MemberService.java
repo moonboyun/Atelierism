e7c5233a31eb7d1965bdd67a80c91ab3ddb20851
조성빈 <binny0903@gmail.com>
2025-10-16 14:19:54 +0900

    [Feat]컨트롤러에서 받은 객체 정보를 담아서 Dao로 전송

--- final_Atelierism_back/src/main/java/kr/co/iei/member/model/service/MemberService.java
+++ final_Atelierism_back/src/main/java/kr/co/iei/member/model/service/MemberService.java
@@ -4,9 +4,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import kr.co.iei.member.model.dao.MemberDao;
+import kr.co.iei.member.model.dto.MemberDTO;
 
 @Service
 public class MemberService {
 	@Autowired
 	private MemberDao memberDao;
+
+	public MemberDTO selectOneMember(String memberId) {
+		System.out.println(memberId);
+		MemberDTO member = memberDao.selectOneMember(memberId);
+		System.out.println(member);
+		member.setMemberPw(null);
+		return member;
+	}
 }
