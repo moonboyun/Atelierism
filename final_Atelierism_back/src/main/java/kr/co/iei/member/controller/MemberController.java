8f667db355577eb71096df206ebc051b28e51e91
조성빈 <binny0903@gmail.com>
2025-10-16 14:19:29 +0900

    [Feat]Backend 컨트롤러에 프론트에서 받은 객체 전송하는 로직 작성

--- final_Atelierism_back/src/main/java/kr/co/iei/member/controller/MemberController.java
+++ final_Atelierism_back/src/main/java/kr/co/iei/member/controller/MemberController.java
@@ -1,10 +1,14 @@
 package kr.co.iei.member.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import kr.co.iei.member.model.dto.MemberDTO;
 import kr.co.iei.member.model.service.MemberService;
 
 @CrossOrigin("*")
@@ -13,5 +17,13 @@ import kr.co.iei.member.model.service.MemberService;
 public class MemberController {
 	@Autowired
 	private MemberService memberService;
+	
+	@GetMapping(value="/{memberId}")
+	public ResponseEntity<MemberDTO> selectOneMember(@PathVariable String memberId){
+		System.out.println(memberId);
+		MemberDTO member = memberService.selectOneMember(memberId);
+		System.out.println(member);
+		return ResponseEntity.ok(member);
+	}
 
 }
