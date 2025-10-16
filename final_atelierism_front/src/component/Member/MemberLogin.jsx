0333b5d0e093d36b0de315767605260a1ef27b3b
조성빈 <binny0903@gmail.com>
2025-10-16 14:18:55 +0900

    [Feat]로그인페이지에서 로그인할때 로그인하는 회원 정보를 저장할 로직 작성중

--- final_atelierism_front/src/component/Member/MemberLogin.jsx
+++ final_atelierism_front/src/component/Member/MemberLogin.jsx
@@ -1,10 +1,50 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./member.css";
+import { useRecoilState } from "recoil";
+import { useState } from "react";
+import { loginIdState, memberTypeState } from "../utils/RecoilData";
+import axios from "axios";
 const MemberLogin = () => {
+  //RecoilData에서 선언한 데이터
+  const [memberId, setMemberId] = useRecoilState(loginIdState); //로그인한 회원 아이디
+  const [memberType, setMemberType] = useRecoilState(memberTypeState); //로그인한 회원 등급
+  const [member, setMember] = useState({
+    //로그인창 들어갈 때 아이디 비밀번호 입력칸 빈 객체로 설정
+    memberId: "",
+    memberPw: "",
+  });
+  const inputMemberData = (e) => {
+    const name = e.target.name; //아이디랑 비밀번호 속성 값
+    const value = e.target.value; //입력된 값
+    const newMember = { ...member, [name]: value }; //기존 멤버객체 복사 후
+    setMember(newMember); //새로운 멤버 객체로 업데이트
+    console.log(newMember);
+  };
+  const navigate = useNavigate();
+  const login = () => {
+    if (member.memberId !== "" && member.memberPw !== "") {
+      const backServer = import.meta.env.VITE_BACK_SERVER;
+      axios
+        .post(`${backServer}/member/login`, member)
+        .then((res) => {
+          setMemberId(res.data.memberId); //로그인한 회원 아이디 정보
+          setMemberType(res.data.memberType); //로그인한 회원 등급 정보
+          navigate("/");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  };
   return (
     <section className="login-wrap">
       <div className="page-title">로그인</div>
-      <form>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          login();
+        }}
+      >
         <div className="input-wrap">
           <div className="input-title">
             <label htmlFor="memberId">아이디</label>
@@ -15,6 +55,8 @@ const MemberLogin = () => {
               name="memberId"
               id="memberId"
               placeholder="아이디를 입력하세요"
+              value={member.memberId}
+              onChange={inputMemberData}
             />
           </div>
         </div>
@@ -29,12 +71,14 @@ const MemberLogin = () => {
               name="memberPw"
               id="memberPw"
               placeholder="비밀번호를 입력하세요"
+              value={member.memberPw}
+              onChange={inputMemberData}
             />
           </div>
         </div>
         <div className="login-button">
           <button type="submit" className="btn-primary lg">
-            로그인
+            <Link to="/">로그인</Link>
           </button>
         </div>
         <div className="member-link-box">
