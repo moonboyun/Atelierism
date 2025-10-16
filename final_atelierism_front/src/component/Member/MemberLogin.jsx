d1bcf4e3390cedf930976fdff8e208a0616d0f67
조성빈 <binny0903@gmail.com>
2025-10-16 15:59:05 +0900

    [Docs]MemberLogin에 로그인 실패시 띄워줄 로직 작성 완료

--- final_atelierism_front/src/component/Member/MemberLogin.jsx
+++ final_atelierism_front/src/component/Member/MemberLogin.jsx
@@ -4,6 +4,7 @@ import { useRecoilState } from "recoil";
 import { useState } from "react";
 import { loginIdState, memberTypeState } from "../utils/RecoilData";
 import axios from "axios";
+import Swal from "sweetalert2";
 const MemberLogin = () => {
   //RecoilData에서 선언한 데이터
   const [memberId, setMemberId] = useRecoilState(loginIdState); //로그인한 회원 아이디
@@ -21,7 +22,9 @@ const MemberLogin = () => {
     console.log(newMember);
   };
   const navigate = useNavigate();
+
   const login = () => {
+    console.log(member);
     if (member.memberId !== "" && member.memberPw !== "") {
       const backServer = import.meta.env.VITE_BACK_SERVER;
       axios
@@ -33,7 +36,17 @@ const MemberLogin = () => {
         })
         .catch((err) => {
           console.log(err);
+          Swal.fire({
+            title: "로그인 실패",
+            text: "아이디 또는 비밀번호를 입력하세요",
+            icon: "warning",
+          });
         });
+    } else {
+      Swal.fire({
+        text: "아이디 또는 비밀번호를 입력하세요.",
+        icon: "info",
+      });
     }
   };
   return (
@@ -78,7 +91,7 @@ const MemberLogin = () => {
         </div>
         <div className="login-button">
           <button type="submit" className="btn-primary lg">
-            <Link to="/">로그인</Link>
+            로그인
           </button>
         </div>
         <div className="member-link-box">
