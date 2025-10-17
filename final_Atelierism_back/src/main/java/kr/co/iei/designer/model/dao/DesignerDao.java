3c5eb73167e41b9e3f014793fa049743a7636451
snow520520 <leeseolhang0624@gmail.com>
2025-10-17 15:59:57 +0900

    [Docs]spring에서 designer테이블 조회 코드 추가
    spring에서 designer의 controller, service, dao, mapper, dto에 코드를 추가하여 모든 디자이너를 조회

--- final_Atelierism_back/src/main/java/kr/co/iei/designer/model/dao/DesignerDao.java
+++ final_Atelierism_back/src/main/java/kr/co/iei/designer/model/dao/DesignerDao.java
@@ -1,5 +1,12 @@
 package kr.co.iei.designer.model.dao;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
 public interface DesignerDao {
+
+	List selectAllDesigner();
 
 }
