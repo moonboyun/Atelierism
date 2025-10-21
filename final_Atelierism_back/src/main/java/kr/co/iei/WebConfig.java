4df3c63c6c23a00a757e70be7993a8c01cc81931
조성빈 <binny0903@gmail.com>
2025-10-21 14:00:51 +0900

    [Fix]WebConfig 로직과 import 겹침 현상 해결

--- final_Atelierism_back/src/main/java/kr/co/iei/WebConfig.java
+++ final_Atelierism_back/src/main/java/kr/co/iei/WebConfig.java
@@ -1,39 +1,29 @@
 package kr.co.iei;
 
-
+		import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+		import org.springframework.context.annotation.Configuration;
+		import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+		import org.springframework.web.socket.config.annotation.EnableWebSocket;
+		import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+		import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 @Configuration
 @EnableWebSocket
 public class WebConfig implements WebMvcConfigurer, WebSocketConfigurer{
+	@Value("${file.root}")
+	private String root;
 	@Bean
 	public BCryptPasswordEncoder bCrypt() {
 		return new BCryptPasswordEncoder();
 	}
-	
 	@Override
 	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
 		//registry
 			//.setAllowedOrigins("*");
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-
-@Configuration
-@EnableWebSocket
-public class WebConfig implements WebMvcConfigurer{
-	@Value("${file.root}")
-	private String root;
-	
+	}
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 	    registry.addResourceHandler("/board/review/**")
