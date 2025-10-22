12a66a7ddabc7c2f54bf2f688b146462d7e399e6
snow520520 <leeseolhang0624@gmail.com>
2025-10-22 14:43:19 +0900

    [Docs]인테리어 정보 bd에 저장

--- final_Atelierism_back/src/main/java/kr/co/iei/interior/controller/InteriorController.java
+++ final_Atelierism_back/src/main/java/kr/co/iei/interior/controller/InteriorController.java
@@ -1,10 +1,17 @@
 package kr.co.iei.interior.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import kr.co.iei.admin.model.dto.PriceListDto;
+import kr.co.iei.admin.model.service.AdminService;
+import kr.co.iei.interior.model.dto.InteriorDTO;
 import kr.co.iei.interior.model.service.InteriorService;
 
 @CrossOrigin("*")
@@ -14,5 +21,19 @@ public class InteriorController {
 	
 	@Autowired
 	private InteriorService interiorService;
+	
+	@Autowired
+	private AdminService adminService;
+	
+	@GetMapping
+	public ResponseEntity<PriceListDto> selectPrice(){
+		PriceListDto price = adminService.priceListSelect();
+		return ResponseEntity.ok(price);
+	}
+	@PostMapping
+	public ResponseEntity<Integer> insertInterior(@RequestBody InteriorDTO interior ){
+		int result = interiorService.insertInterior(interior);
+		return ResponseEntity.ok(result);
+	}
 
 }
--- final_Atelierism_back/src/main/java/kr/co/iei/interior/model/service/InteriorService.java
+++ final_Atelierism_back/src/main/java/kr/co/iei/interior/model/service/InteriorService.java
@@ -2,12 +2,20 @@ package kr.co.iei.interior.model.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import kr.co.iei.interior.model.dao.InteriorDao;
+import kr.co.iei.interior.model.dto.InteriorDTO;
 
 @Service
 public class InteriorService {
 	
 	@Autowired
 	private InteriorDao interiorDao;
+	
+	@Transactional
+	public int insertInterior(InteriorDTO interior) {
+		int result = interiorDao.insertInterior(interior);
+		return result;
+	}
 }
