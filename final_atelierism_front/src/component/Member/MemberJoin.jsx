ac1d7601f62bda3f2ffc6376ee90be4e9eab755b
조성빈 <binny0903@gmail.com>
2025-10-23 15:14:06 +0900

    [Docs]MemberJoin 컴포넌트에 발견 못한 console.log 제거

--- final_atelierism_front/src/component/Member/MemberJoin.jsx
+++ final_atelierism_front/src/component/Member/MemberJoin.jsx
@@ -117,7 +117,6 @@ const MemberJoin = () => {
       zonecode: data.zonecode,
       address: data.address,
     });
-    console.log(memberAddr.address);
     closeModal();
     setMember({ ...member, memberAddr: data.address });
   };
