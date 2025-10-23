f7715b58f271d12c22df49c3c2611ff0c9586b61
조성빈 <binny0903@gmail.com>
2025-10-23 14:07:55 +0900

    [Docs]MemberController에 이메일 인증 로직 추가

--- final_Atelierism_back/src/main/java/kr/co/iei/member/controller/MemberController.java
+++ final_Atelierism_back/src/main/java/kr/co/iei/member/controller/MemberController.java
@@ -1,5 +1,7 @@
 package kr.co.iei.member.controller;
 
+import java.util.Random;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -12,11 +14,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import kr.co.iei.member.model.dto.LoginMemberDTO;
 import kr.co.iei.member.model.dto.MemberDTO;
 import kr.co.iei.member.model.service.MemberService;
+import kr.co.iei.util.EmailSender;
 import kr.co.iei.util.JwtUtils;
 
 @CrossOrigin("*")
@@ -27,6 +31,9 @@ public class MemberController {
 	private MemberService memberService;
 	@Autowired
 	private JwtUtils Jwt;
+	@Autowired
+	private EmailSender mailSender;
+	
 	@PostMapping
 	public ResponseEntity<Integer> insertMember(@RequestBody MemberDTO member){
 		System.out.println("컨트롤러 멤버 : "+member);
@@ -83,4 +90,50 @@ public class MemberController {
 		loginMember.setRefreshToken(refreshToken);
 		return ResponseEntity.ok(loginMember);
 	}
+	
+	@GetMapping(value="/sendCode")
+	@ResponseBody
+	public String sendCode(@RequestParam String memberEmail) {
+		//인증 메일용 제목 생성
+		String emailTitle = "Atelierism 인증메일입니다.";
+		System.out.println(emailTitle);
+		//인증코드 생성
+		Random r = new Random();
+		StringBuffer sb = new StringBuffer();
+		for(int i=0;i<6;i++) {
+			//대문자 : r.nextInt(26)+65
+			//소문자: r.nextInt(26)+97
+			//숫자: r.nextInt(10)
+			int flag = r.nextInt(3); //0:숫자, 1:대문자, 2:소문자
+			System.out.println(flag);
+			System.out.println(r);
+			if(flag==0) {
+				int randomCode = r.nextInt(10);
+				sb.append(randomCode);
+				System.out.println(sb);
+				System.out.println(randomCode);
+			}else if(flag==1) {
+				char randomCode = (char)(r.nextInt(26)+65);
+				sb.append(randomCode);
+				System.out.println(sb);
+				System.out.println(randomCode);
+			}else if(flag==2) {
+				char randomCode = (char)(r.nextInt(26)+97);
+				sb.append(randomCode);
+				System.out.println(sb);
+				System.out.println(randomCode);
+			}
+		}//for
+		String emailContent = "안녕하세요 Atelierism입니다.";
+		emailContent += "인증번호는 [";
+		
+		emailContent += sb.toString();
+
+		emailContent += "]입니다";
+		mailSender.sendMail(emailTitle, memberEmail, emailContent);
+		System.out.println(emailTitle);
+		System.out.println(memberEmail);
+		System.out.println(emailContent);
+		return sb.toString();
+	}//sendCode
 }
