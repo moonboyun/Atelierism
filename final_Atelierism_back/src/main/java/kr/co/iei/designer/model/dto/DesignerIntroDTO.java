dd1b019503674bd681b117276c5b8668f9e3d44c
snow520520 <leeseolhang0624@gmail.com>
2025-10-28 21:53:44 +0900

    [Docs]어노테이션 추가, 수정

--- final_Atelierism_back/src/main/java/kr/co/iei/designer/model/dto/DesignerIntroDTO.java
+++ final_Atelierism_back/src/main/java/kr/co/iei/designer/model/dto/DesignerIntroDTO.java
@@ -2,15 +2,18 @@ package kr.co.iei.designer.model.dto;
 
 import org.apache.ibatis.type.Alias;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
-@Alias("designerIntro")
+@Alias(value="designerIntro")
 public class DesignerIntroDTO {
 
 	private String memberId;
     private String memberName;
     private String memberThumb; 
-
     private String designerIntroduce;
 }
