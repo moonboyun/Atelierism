cda7d4195ed8a2624169562053f2189ba15641d0
hybye <hyb6019@gmail.com>
2025-10-28 16:03:13 +0900

    [Docs] DesignerDetailDTO 추가

---
+++ final_Atelierism_back/src/main/java/kr/co/iei/designer/model/dto/DesignerDetailDTO.java
@@ -0,0 +1,31 @@
+package kr.co.iei.designer.model.dto;
+
+import java.util.List;
+
+import org.apache.ibatis.type.Alias;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Alias(value="designerDetailDTO")
+public class DesignerDetailDTO {
+    private String memberId;
+    private String memberName;
+    private String memberThumb;
+    private int designerCareer;
+    private int designerAge;
+    private String designerGraduationDate;
+    private String designerGraduation;
+    private String designerPortfolio;
+    private String designerChat;
+    private String designerIntroduce;
+
+    private List<CareerDetailDTO> careerList;
+
+    private List<AwardsCareerDTO> awardList;
+}
+
