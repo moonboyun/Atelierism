7df4182296fa776c03b7e2ea9a8d9d38012f7d5d
hybye <hyb6019@gmail.com>
2025-10-29 17:02:31 +0900

    [Docs] DesinerDao 매소드 추가
    statusTotalCount, selectStatusList, selectStatusDetail, updateStatus 매소드 생성

--- final_Atelierism_back/src/main/java/kr/co/iei/designer/model/dao/DesignerDao.java
+++ final_Atelierism_back/src/main/java/kr/co/iei/designer/model/dao/DesignerDao.java
@@ -1,6 +1,7 @@
 package kr.co.iei.designer.model.dao;
 
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
 
@@ -9,6 +10,8 @@ import kr.co.iei.designer.model.dto.CareerDetailDTO;
 import kr.co.iei.designer.model.dto.DesignerDTO;
 import kr.co.iei.designer.model.dto.DesignerDetailDTO;
 import kr.co.iei.designer.model.dto.DesignerIntroDTO;
+import kr.co.iei.designer.model.dto.DesignerStatusDTO;
+import kr.co.iei.designer.model.dto.DesignerStatusDetailDTO;
 import kr.co.iei.util.PageInfo;
 
 @Mapper
@@ -29,5 +32,11 @@ public interface DesignerDao {
     List<AwardsCareerDTO> selectDesignerAwards(String memberId);
 
 	List selectDesignerBoard();
-
+	
+	int statusTotalCount(String designerId);
+    List<DesignerStatusDTO> selectStatusList(PageInfo pi, String designerId);
+    
+    DesignerStatusDetailDTO selectStatusDetail(int interiorNo);
+    
+    int updateStatus(Map<String, Object> params);
 }
