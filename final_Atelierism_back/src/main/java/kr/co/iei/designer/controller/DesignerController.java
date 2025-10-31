3634601ab9f5eb0a880de1b20605b9131e916be6
hybye <hyb6019@gmail.com>
2025-10-31 14:59:26 +0900

    [Docs] 차트 매소드 추가

--- final_Atelierism_back/src/main/java/kr/co/iei/designer/controller/DesignerController.java
+++ final_Atelierism_back/src/main/java/kr/co/iei/designer/controller/DesignerController.java
@@ -15,21 +15,17 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import kr.co.iei.designer.model.dto.AwardsCareerDTO;
-import kr.co.iei.designer.model.dto.CareerDetailDTO;
 import kr.co.iei.designer.model.dto.DesignerApplyRequestDTO;
-import kr.co.iei.designer.model.dto.DesignerDTO;
+import kr.co.iei.designer.model.dto.DesignerChartDTO;
 import kr.co.iei.designer.model.dto.DesignerDetailDTO;
-import kr.co.iei.designer.model.dto.DesignerIntroDTO;
+
 import kr.co.iei.designer.model.dto.DesignerStatusDetailDTO;
 import kr.co.iei.designer.model.dto.MemberDesignerDTO;
 import kr.co.iei.designer.model.service.DesignerService;
@@ -141,4 +137,9 @@ public class DesignerController {
         return ResponseEntity.ok(result);
     }
 	
+	@GetMapping("/chart/{designerId}")
+    public ResponseEntity<List<DesignerChartDTO>> selectChartData(@PathVariable String designerId) {
+        List<DesignerChartDTO> chartData = designerService.selectChartData(designerId);
+        return ResponseEntity.ok(chartData);
+    }
 }
