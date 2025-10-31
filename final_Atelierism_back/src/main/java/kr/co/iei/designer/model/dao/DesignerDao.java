5fbac2ead4b7d6ca871acf39163552ed0b9e596a
hybye <hyb6019@gmail.com>
2025-10-31 14:59:44 +0900

    [Docs] 안쓰는 import 구문 삭제

--- final_Atelierism_back/src/main/java/kr/co/iei/designer/model/dao/DesignerDao.java
+++ final_Atelierism_back/src/main/java/kr/co/iei/designer/model/dao/DesignerDao.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
 
 import kr.co.iei.designer.model.dto.AwardsCareerDTO;
 import kr.co.iei.designer.model.dto.CareerDetailDTO;
