de5a268882344f3fb6c1ec7ec7be9c42d02b5d03
snow520520 <leeseolhang0624@gmail.com>
2025-11-01 23:29:09 +0900

    [Feat]모달창에서 새로고침 시 alter 띄우는 기능 추가
    모달창에서 새로고침하면 꺼지는 걸 alter을 띄워 저장되지 않는 다는 경고문을 띄우고 그래도 새로고침 버튼 누르면 새로고침 되어 모달창이 닫히고 취소 누르면 새로고침 안되는 기능 추가

--- final_atelierism_front/src/component/interior/InteriorApplication.jsx
+++ final_atelierism_front/src/component/interior/InteriorApplication.jsx
@@ -159,7 +159,7 @@ const InteriorApplication = ({ onClose, ani, setAni, setIsInterior }) => {
         icon: "warning",
         reverseButtons: true,
         showCancelButton: true,
-        cancelButtonText: "닫기",
+        cancelButtonText: "머무르기",
         confirmButtonText: "나가기",
         confirmButtonColor: " #8aa996",
       }).then((select) => {
@@ -173,6 +173,34 @@ const InteriorApplication = ({ onClose, ani, setAni, setIsInterior }) => {
       document.body.style.overflow = "auto";
     }
   };
+
+  const usePreventRefresh = () => {
+// 1. custom hook으로 사용할 함수를 하나 생성한다.
+  const preventClose = (e) => {
+  // 2. 해당 함수 안에 새로운 함수를 생성하는데, 이때 이 함수는 자바스크립트의 이벤트를 감지하게된다.
+    e.preventDefault();
+    // 2-1. 특정 이벤트에 대한 사용자 에이전트 (브라우저)의 기본 동작이 실행되지 않도록 막는다.
+    e.returnValue = ''; 
+    // 2-2. e.preventDefault를 통해서 방지된 이벤트가 제대로 막혔는지 확인할 때 사용한다고 한다.
+    // 2-3. 더 이상 쓰이지 않지만, chrome 설정상 필요하다고 하여 추가함.
+    // 2-4. returnValue가 true일 경우 이벤트는 그대로 실행되고, false일 경우 실행되지 않는다고 한다.
+  };
+
+  // 브라우저에 렌더링 시 한 번만 실행하는 코드
+  useEffect(() => {
+    (() => {
+      window.addEventListener('beforeunload', preventClose);
+      // 4. beforeunload 이벤트는 리소스가 사라지기 전 window 자체에서 발행한다.
+      // 4-2. window의 이벤트를 감지하여 beforunload 이벤트 발생 시 preventClose 함수가 실행된다.
+    })();
+
+    return () => {
+      window.removeEventListener('beforeunload', preventClose);
+      // 5. 해당 이벤트 실행 후, beforeunload를 감지하는 것을 제거한다.
+    };
+  });
+};
+
   return (
     <section className="inter-section">
       <div className={ani ? "inter-content active" : "inter-content"}>
@@ -196,6 +224,7 @@ const InteriorApplication = ({ onClose, ani, setAni, setIsInterior }) => {
           )}
         </div>
       </div>
+      {usePreventRefresh()}
     </section>
   );
 };
